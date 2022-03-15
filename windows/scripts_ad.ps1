d10d6d3f1c2a12d67d4b2719fa51ad82afbd3b9c
kerjox <kerjoxgamer095@gmail.com>
2022-03-15 13:00:02 +0100

    Pruebas

--- windows/scripts_ad.ps1
+++ windows/scripts_ad.ps1
@@ -2,6 +2,6 @@ $dominioLDAP="DC=aragon,DC=local"
 
 New-ADOrganizationalUnit -DisplayName "USUARIOS" -Name "USUARIOS" -ProtectedFromAccidentalDeletion $False -path $dominioLDAP
 
-New-ADOrganizationalUnit -DisplayName "Gestores" -Name "Gestores" -ProtectedFromAccidentalDeletion $False -path "OU=USUARIOS,+$dominioLDAP"
+New-ADOrganizationalUnit -DisplayName "Gestores" -Name "Gestores" -ProtectedFromAccidentalDeletion $False -path "OU=USUARIOS,$dominioLDAP"
 New-ADOrganizationalUnit -DisplayName "Tutores" -Name "Tutores" -ProtectedFromAccidentalDeletion $False -path "OU=USUARIOS,$dominioLDAP"
 New-ADOrganizationalUnit -DisplayName "Competidores" -Name "Competidores" -ProtectedFromAccidentalDeletion $False -path "OU=USUARIOS,$dominioLDAP"
