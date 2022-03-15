90597b748e8b521b39f3c7431e991440df3dd9e6
kerjox <kerjoxgamer095@gmail.com>
2022-03-15 13:03:41 +0100

    Priebas

--- windows/scripts_ad.ps1
+++ windows/scripts_ad.ps1
@@ -1,7 +1,15 @@
 $dominioLDAP="DC=aragon,DC=local"
 
+
+# Creacion de las unidades organizativas
 New-ADOrganizationalUnit -DisplayName "USUARIOS" -Name "USUARIOS" -ProtectedFromAccidentalDeletion $False -path $dominioLDAP
 
 New-ADOrganizationalUnit -DisplayName "Gestores" -Name "Gestores" -ProtectedFromAccidentalDeletion $False -path "OU=USUARIOS,$dominioLDAP"
 New-ADOrganizationalUnit -DisplayName "Tutores" -Name "Tutores" -ProtectedFromAccidentalDeletion $False -path "OU=USUARIOS,$dominioLDAP"
 New-ADOrganizationalUnit -DisplayName "Competidores" -Name "Competidores" -ProtectedFromAccidentalDeletion $False -path "OU=USUARIOS,$dominioLDAP"
+
+# Creacion de los grupos de usuarios
+
+New-ADGroup -DisplayName "Gestores" -Name "Gestores" -Path "OU=Gestores,OU=USUARIOS,$dominioLDAP"
+New-ADGroup -DisplayName "Tutores" -Name "Tutores" -Path "OU=Tutores,OU=USUARIOS,$dominioLDAP"
+New-ADGroup -DisplayName "Competidores" -Name "Competidores" -GroupCategory Security -Path "OU=Competidores,OU=USUARIOS,$dominioLDAP"
