bf9744b12706cae291f1c75a4f9e44cbcc91b301
kerjox <kerjoxgamer095@gmail.com>
2022-03-15 12:27:56 +0100

    Pruebas

--- windows/scripts_ad.ps1
+++ windows/scripts_ad.ps1
@@ -2,6 +2,6 @@ $dominioLDAP="DC=aragon,DC=local"
 
 New-ADOrganizationalUnit -DisplayName "USUARIOS" -Name "USUARIOS" -path $dominioLDAP
 
-New-ADOrganizationalUnit -DisplayName "Gestores" -Name "Gestores" -path "OU=USUARIOS,"+$dominioLDAP
-New-ADOrganizationalUnit -DisplayName "Tutores" -Name "Tutores" -path "OU=USUARIOS,"+$dominioLDAP
-New-ADOrganizationalUnit -DisplayName "Competidores" -Name "Competidores" -path "OU=USUARIOS,"+$dominioLDAP
+New-ADOrganizationalUnit -DisplayName "Gestores" -Name "Gestores" -path "OU=USUARIOS,+$dominioLDAP"
+New-ADOrganizationalUnit -DisplayName "Tutores" -Name "Tutores" -path "OU=USUARIOS,$dominioLDAP"
+New-ADOrganizationalUnit -DisplayName "Competidores" -Name "Competidores" -path "OU=USUARIOS,$dominioLDAP"
