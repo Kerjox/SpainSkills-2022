22de97e7001317e298efd32570a3cb66ba2790d3
kerjox <kerjoxgamer095@gmail.com>
2022-03-15 13:50:42 +0100

    Pruebas

--- windows/scripts_ad.ps1
+++ windows/scripts_ad.ps1
@@ -1,4 +1,6 @@
-$dominioLDAP="DC=aragon,DC=local"
+$dominioLDAP="DC=dom,DC=as21"
+$domain_name="dom.as21"
+$userPass=(convertto-securestring "IVSZ2e12" -AsPlainText -Force)
 
 
 # Creacion de las unidades organizativas
@@ -10,6 +12,29 @@ New-ADOrganizationalUnit -DisplayName "Competidores" -Name "Competidores" -Prote
 
 # Creacion de los grupos de usuarios
 
-New-ADGroup -DisplayName "Gestores" -Name "Gestores" -Path "OU=Gestores,OU=USUARIOS,$dominioLDAP"
-New-ADGroup -DisplayName "Tutores" -Name "Tutores" -Path "OU=Tutores,OU=USUARIOS,$dominioLDAP"
-New-ADGroup -DisplayName "Competidores" -Name "Competidores" -GroupCategory Security -Path "OU=Competidores,OU=USUARIOS,$dominioLDAP"
+New-ADGroup -DisplayName "Gestores" -Name "Gestores" -GroupScope Global -GroupCategory Security -Path "OU=Gestores,OU=USUARIOS,$dominioLDAP"
+New-ADGroup -DisplayName "Tutores" -Name "Tutores" -GroupScope Global -GroupCategory Security -Path "OU=Tutores,OU=USUARIOS,$dominioLDAP"
+New-ADGroup -DisplayName "Competidores" -Name "Competidores" -GroupScope Global -GroupCategory Security -Path "OU=Competidores,OU=USUARIOS,$dominioLDAP"
+
+
+
+for (($i = 1); $i -lt 11 ; $i++)
+{
+    $name="Gestor$i"
+    New-ADUser -Name $name -UserPrincipalName "$name@$domain_name" -PasswordNeverExpires $True -CannotChangePassword $True -Enabled $True -AccountPassword $userPass -Path "OU=Gestores,OU=USUARIOS,$dominioLDAP"
+    Add-ADGroupMember "Gestores" $name
+}
+
+for (($i = 1); $i -lt 11 ; $i++)
+{
+    $name="Tutor$i"
+    New-ADUser -Name $name -UserPrincipalName "$name@$domain_name" -PasswordNeverExpires $True -CannotChangePassword $True -Enabled $True -AccountPassword $userPass -Path "OU=Tutores,OU=USUARIOS,$dominioLDAP"
+    Add-ADGroupMember "Tutores" $name
+}
+
+for (($i = 1); $i -lt 11 ; $i++)
+{
+    $name="Competidor$i"
+    New-ADUser -Name $name -UserPrincipalName "$name@$domain_name" -PasswordNeverExpires $True -CannotChangePassword $True -Enabled $True -AccountPassword $userPass -Path "OU=Competidores,OU=USUARIOS,$dominioLDAP"
+    Add-ADGroupMember "Competidores" $name
+}
